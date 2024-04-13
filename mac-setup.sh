77f7cbd7b7b70f3b9877719b92f4ab7540948617
Tammy Liu <tammy.liu.x@gmail.com>
2024-04-12 22:30:38 -0400

    Screencapture redirect

--- mac-setup.sh
+++ mac-setup.sh
@@ -2,3 +2,7 @@ ln -s $HOME/mac-setup/dotfiles/zshrc $HOME/.zshrc
 ln -s $HOME/mac-setup/dotfiles/gitconfig $HOME/.gitconfig
 
 brew bundle
+
+mkdir $HOME/Screencapture
+defaults write com.apple.screencapture location $HOME/Screenshots
+killall SystemUIServer
