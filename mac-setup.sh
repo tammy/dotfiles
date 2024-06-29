c9fe9e872e30c199799a8e9ebfad1ce9b87a5413
Tammy Liu <tammy.liu.x@gmail.com>
2024-06-29 19:26:24 -0400

    Change copilot to gh

--- mac-setup.sh
+++ mac-setup.sh
@@ -1,8 +1,12 @@
+# Link configs
 ln -s $HOME/mac-setup/dotfiles/zshrc $HOME/.zshrc
 ln -s $HOME/mac-setup/dotfiles/gitconfig $HOME/.gitconfig
 
+# Save screenshots to a special folder
+mkdir $HOME/Screencapture
+defaults write com.apple.screencapture location $HOME/Screencapture
+killall SystemUIServer
+
+# Install
 brew bundle
 
-mkdir $HOME/Screencapture
-defaults write com.apple.screencapture location $HOME/Screenshots
-killall SystemUIServer
